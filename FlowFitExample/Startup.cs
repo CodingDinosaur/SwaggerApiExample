cb6693eb2a46960961f35211e1cacddf692645c4
Gilbert Samuelian <gsamuelian@blizzard.com>
2019-11-18 20:57:57 -0800

    Add Angular site

--- FlowFitExample/Startup.cs
+++ FlowFitExample/Startup.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace FlowFitExample
 {
@@ -12,6 +12,7 @@ namespace FlowFitExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSpaStaticFiles(o => o.RootPath = "ClientApp/dist/ClientApp");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,14 +24,9 @@ namespace FlowFitExample
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa => { spa.Options.SourcePath = "ClientApp"; });
         }
     }
 }
