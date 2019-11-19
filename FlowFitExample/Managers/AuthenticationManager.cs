7a1971243bc7a62a6d158c2d9fd898b5e1b1d984
Gilbert Samuelian <gsamuelian@blizzard.com>
2019-11-18 19:35:31 -0800

    Add project files.

---
+++ FlowFitExample/Managers/AuthenticationManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FlowFitExample.Models;
+using Microsoft.Extensions.Logging;
+
+namespace FlowFitExample.Managers
+{
+    public class AuthenticationManager : IAuthenticationManager
+    {
+        private ILogger<AuthenticationManager> _logger;
+        
+
+        public Task<bool> Authenticate(LoginRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClaimsPrincipal> GetClaimsPrincipalAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetToken(ClaimsPrincipal principal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlowFitExample/Managers/IAuthenticationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FlowFitExample.Models;
+
+namespace FlowFitExample.Managers
+{
+    interface IAuthenticationManager
+    {
+        Task<bool> Authenticate(LoginRequest loginRequest);
+        Task<ClaimsPrincipal> GetClaimsPrincipalAsync(string userName);
+        string GetToken(ClaimsPrincipal principal);
+    }
+}
