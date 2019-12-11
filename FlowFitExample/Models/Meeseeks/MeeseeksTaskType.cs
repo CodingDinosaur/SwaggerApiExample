85424bb56d33dfbc3e62d325ab42ec0308337b7d
Gilbert Samuelian <gsamuelian@blizzard.com>
2019-12-10 22:23:36 -0800

    Serialize task type as string
    Provides wire safety, and allows the enum values to be represented in the Swagger Schema documentation

--- FlowFitExample/Models/Meeseeks/MeeseeksTaskType.cs
+++ FlowFitExample/Models/Meeseeks/MeeseeksTaskType.cs
@@ -1,5 +1,9 @@
-﻿namespace FlowFitExample.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FlowFitExample.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum MeeseeksTaskType
     {
         Unknown,
