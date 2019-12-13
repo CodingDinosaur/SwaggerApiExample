4f55054a4038991043eb8ea71d7cb4a1257c2544
Gilbert Samuelian <gsamuelian@blizzard.com>
2019-12-12 16:57:15 -0800

    Added more documentation

--- FlowFitExample/Models/Platypus.cs
+++ FlowFitExample/Models/Platypus.cs
@@ -1,5 +1,8 @@
 ﻿namespace FlowFitExample.Models
 {
+    /// <summary>
+    /// An odd creature
+    /// </summary>
     public class Platypus
     {
         public Platypus(string identifier, long billLength, long billWidth, long billDisplacement, long billPowerCoeffecient)
@@ -11,10 +14,29 @@
             BillPowerCoeffecient = billPowerCoeffecient;
         }
 
+        /// <summary>
+        /// String identifier for this Platypus
+        /// </summary>
         public string Identifier { get; set; }
+        
+        /// <summary>
+        /// Length of the animal's bill
+        /// </summary>
         public long BillLength { get; set; }
+        
+        /// <summary>
+        /// Width of the animail's bill
+        /// </summary>
         public long BillWidth { get; set; }
+        
+        /// <summary>
+        /// Displacement of the bill if it was submerged in water
+        /// </summary>
         public long BillDisplacement { get; set; }
+        
+        /// <summary>
+        /// Universal bill power coeffecient that grants the Platypus its magic
+        /// </summary>
         public long BillPowerCoeffecient { get; set; }
     }
 }
--- FlowFitExample/Models/StringSegment.cs
+++ FlowFitExample/Models/StringSegment.cs
@@ -2,6 +2,9 @@
 
 namespace FlowFitExample.Models
 {
+    /// <summary>
+    /// A block of text with localization information
+    /// </summary>
     public class StringSegment
     {
         public StringSegment(string language, string value)
@@ -11,9 +14,24 @@ namespace FlowFitExample.Models
             Value = value;
         }
 
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
         public Guid Id { get; set; }
+        
+        /// <summary>
+        /// Language of the text
+        /// </summary>
         public string Language { get; set; }
+        
+        /// <summary>
+        /// Underlying text value
+        /// </summary>
         public string Value { get; set; }
+        
+        /// <summary>
+        /// Length of the text value
+        /// </summary>
         public int Length => Value.Length;
     }
 }
